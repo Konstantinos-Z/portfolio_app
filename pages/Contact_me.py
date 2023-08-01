a4bfcdf73b73ee30d50923a03ed1b47e7d021b03
Konstantinos <zigouris.konstantinos@gmail.com>
2023-08-01 19:49:42 +0300

    Fix a typo

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -4,7 +4,7 @@ from send_email import send_email
 st.header("Contact me")
 
 with st.form(key="email_forms"):
-    user_email = st.text_input("Your email adress")
+    user_email = st.text_input("Your email address")
     raw_message = st.text_area("Your message")
     message = f"""\
 Subject: New email from {user_email}
