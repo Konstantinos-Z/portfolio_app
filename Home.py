e5c8c712cb60730443855b40b5b05da4f82710ce
Konstantinos <zigouris.konstantinos@gmail.com>
2023-08-13 18:41:35 +0300

    Make minor changes to the Home page

--- Home.py
+++ Home.py
@@ -11,31 +11,24 @@ with col1:
 with col2:
     st.title("Konstantinos Zygouris")
     content = """
-    Hello, I'm Konstantinos Zygouris and I am from Greece! I am currently learning how to code in python
-    so i will be ready to start attending a computer science course in the Netherlands at the University of Twente
-    by September 2023.I enjoy many different things such as extreme sports, driving, programming, and gaming.
+    Hello, I'm Konstantinos Zygouris and I am from Greece! Below is a selection of some of the apps i have built during the summer of 2023 with the help of a python course I took. I created these apps in preparation for beginning a Bachelor of Computer Science course in the Netherlands at the University of Twente, starting in September 2023.  I enjoy a variety of activities such as extreme sports, driving, programming, watching anime, movies, and gaming. Feel free to contact me!
     """
     st.info(content)
-
-content2 = """
-Below you can find some of the apps I have built in Python.Feel free to contact me!
-"""
-st.write(content2)
 
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=';')
 
 with col3:
-    for index, row in df[:10].iterrows():
+    for index, row in df[:7].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row['image'])
-        st.write(f"[Source Code (some apps are not ready yet!)]({row['url']})")
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index, row in df[10:].iterrows():
+    for index, row in df[7:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row['image'])
-        st.write(f"[Source Code (some apps are not ready yet!)]({row['url']})")
+        st.write(f"[Source Code]({row['url']})")
